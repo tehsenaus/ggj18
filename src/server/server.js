7020a5cac2d0c86d9a838869a9e226c289dd88b8
Sean Micklethwaite <sean@sdmworld.co.uk>
2018-01-27 16:09:41 +0000

    fix server

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,7 @@
 
 import express from 'express';
-import {runGameLoop, runGame, getStateUpdate} from './game';
+import {runGameLoop} from './loop';
+import {runGame} from './game';
 
 const app = express();
 
@@ -9,13 +10,17 @@ const {
     getStateUpdate
 } = runGameLoop(runGame());
 
+setTimeout(() => {
+    sendInput('testId', 'addPlayer', {
+        name: 'Alex'
+    })
+}, 5000);
+
 app.get('/state', async (req, res) => {
     res.json(await getStateUpdate(req.query.id, req.query.seq));
 });
 
 app.post('/player', (req, res) => {
-    const id = nextId++;
-
     const id = sendInput('addPlayer', {
         name: res.query.name
     });
