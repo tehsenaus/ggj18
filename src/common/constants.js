1f9ef1442fa4b8dfdcccf1630c48fc20f79119cb
Sean Micklethwaite <sean@sdmworld.co.uk>
2018-01-27 17:43:05 +0000

    start game button

---
+++ src/common/constants.js
@@ -0,0 +1,2 @@
+
+export const HOST_ID = 'HOST';
--- src/ui/host/HostUi.jsx
+++ src/ui/host/HostUi.jsx
@@ -1,6 +1,16 @@
 import { h, Component } from 'preact';
+import { values } from 'lodash';
+import {HOST_ID} from '../../common/constants';
+
+const clientId = HOST_ID;
 
 export default class App extends Component {
+    startGame = () => {
+        return fetch('/game/start?id=' + clientId, {
+            method: 'POST',
+        });
+    }
+
     componentDidMount() {
         this.setState({ seqNo: -1 });
 
@@ -21,9 +31,38 @@ export default class App extends Component {
     }
 
     render() {
+        const { players } = this.state.game || {};
+
         return (
             <div>
-                { JSON.stringify(this.state) }
+                <p className="text-center">
+                    <h1>MISSION: TRANSMISSION</h1>
+
+                    <button className="btn btn-primary" onClick={this.startGame}>START GAME</button>
+                </p>
+
+                <table className="table table-striped leaderboard">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>NAME</th>
+                            <th>SCORE</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        { values(players).map((player, i) => (
+                            <tr>
+                                <td>#{i+1}</td>
+                                <td>{ player.name }</td>
+                                <td>0</td>
+                            </tr>
+                        )) }
+                    </tbody>
+                </table>
+
+                <pre>
+                    { JSON.stringify(this.state, null, 2) }
+                </pre>
             </div>
         );
     }
