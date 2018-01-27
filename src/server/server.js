c8da4f3ee1344c06c394f9352bc924918447b8d4
Odrinwhite <odrinwhite@gmail.com>
2018-01-27 20:23:21 +0000

    Fixed import in password API

--- src/server/server.js
+++ src/server/server.js
@@ -5,7 +5,7 @@ import express from 'express';
 import {runGameLoop} from './loop';
 import {runGame} from './game';
 import webpackConfig from '../../webpack.client.config';
-import {HOST_ID, ADD_PLAYER_INPUT, START_GAME_INPUT, RESET_GAME_INPUT} from '../common/constants';
+import {HOST_ID, ADD_PLAYER_INPUT, START_GAME_INPUT, RESET_GAME_INPUT, GUESS_PASSWORD_INPUT} from '../common/constants';
 
 const compiler = webpack(webpackConfig);
 const app = express();
