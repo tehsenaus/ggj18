1bb584ed1c8b0a5a629465747f46464245d73491
Sean Micklethwaite <sean@sdmworld.co.uk>
2018-01-27 18:10:15 +0000

    host page states

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,2 +1,24 @@
 
 export const HOST_ID = 'HOST';
+
+/**
+ * Phase when players are signing up, entering their real names.
+ */
+export const LOBBY_PHASE = 'lobby';
+
+/**
+ * Main gameplay phase - players have to find their partner and
+ * exchange passwords.
+ */
+export const INPUT_PASSWORDS_PHASE = 'receivePasswords';
+
+/**
+ * At the end of each round, we show the leaderboard, and count
+ * down to the next round.
+ */
+export const ROUND_END_PHASE = 'roundEnd';
+
+/**
+ * End of the game. All rounds done.
+ */
+export const GAME_END_PHASE = 'gameEnd';
--- src/ui/host/HostUi.jsx
+++ src/ui/host/HostUi.jsx
@@ -1,6 +1,6 @@
 import { h, Component } from 'preact';
 import { values } from 'lodash';
-import {HOST_ID} from '../../common/constants';
+import {HOST_ID, LOBBY_PHASE, INPUT_PASSWORDS_PHASE, ROUND_END_PHASE, GAME_END_PHASE} from '../../common/constants';
 
 const clientId = HOST_ID;
 
@@ -31,39 +31,69 @@ export default class App extends Component {
     }
 
     render() {
-        const { players } = this.state.game || {};
+        const { phase, players } = this.state.game || {};
 
         return (
             <div>
                 <p className="text-center">
                     <h1>MISSION: TRANSMISSION</h1>
 
-                    <button className="btn btn-primary" onClick={this.startGame}>START GAME</button>
+                    { phase === LOBBY_PHASE && <button className="btn btn-primary" onClick={this.startGame}>START GAME</button> }
                 </p>
 
-                <table className="table table-striped leaderboard">
-                    <thead>
-                        <tr>
-                            <th></th>
-                            <th>NAME</th>
-                            <th>SCORE</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        { values(players).map((player, i) => (
-                            <tr>
-                                <td>#{i+1}</td>
-                                <td>{ player.name }</td>
-                                <td>0</td>
-                            </tr>
-                        )) }
-                    </tbody>
-                </table>
+                { phase === LOBBY_PHASE && this.renderPlayerList(players) }
+
+                { phase === INPUT_PASSWORDS_PHASE && this.renderCountdown() }
+
+                { (phase === ROUND_END_PHASE || phase === GAME_END_PHASE) && this.renderLeaderboard(players) }
 
                 <pre>
                     { JSON.stringify(this.state, null, 2) }
                 </pre>
             </div>
+        );
+    }
+
+    renderPlayerList(players) {
+        players = values(players);
+
+        return <p className='text-center'>
+            <h2>{ players.length } player(s) joined:</h2>
+
+            { players.map(player => (
+                <span>{ player.name }</span>
+            )) }
+        </p>
+    }
+
+    renderCountdown() {
+        return (
+            <div>
+                <h2>Find your partner and exchange passwords! GO GO GO!!</h2>
+            </div>
+        )
+    }
+
+    renderLeaderboard(players) {
+        return (
+            <table className="table table-striped leaderboard">
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>NAME</th>
+                        <th>SCORE</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    { values(players).map((player, i) => (
+                        <tr>
+                            <td>#{i+1}</td>
+                            <td>{ player.name }</td>
+                            <td>0</td>
+                        </tr>
+                    )) }
+                </tbody>
+            </table>
         );
     }
 }
