c42cbc9a00f5d11b6ecc6f9ee46444a3e16c1b32
Matt Willett <mattwillettuk@gmail.com>
2018-01-28 15:27:49 +0000

    centred ui

--- src/ui/player/PlayerUi.jsx
+++ src/ui/player/PlayerUi.jsx
@@ -131,7 +131,7 @@ export default class PlayerUi extends Component {
     };
 
     render() {
-        return <div className="ui__player">
+        return <div className="ui__player o-flex">
             { this.renderMain() }
 
             {/* <pre>
@@ -228,7 +228,7 @@ export default class PlayerUi extends Component {
 
     renderLobby(game) {
         if(!game.name){
-            return <div className="ui__lobby">
+            return <div className="o-flex--1 ui__lobby o-flex">
                 <h1>Please enter your name below</h1>
                 <br />
                 <input type={"text"}
@@ -244,28 +244,27 @@ export default class PlayerUi extends Component {
         }
 
           return (
-            <div className="ui__lobby ui__lobby--has-entered">
-            <h1>
-                You are in the lobby, wait until game starts.
-            </h1>
+            <div className="o-flex--1 ui__lobby ui__lobby--has-entered">
 
-            <p>{ game.players.length } player(s) joined:</p>
+                <h2>You are in the lobby, wait until game starts.</h2>
 
-            { game.players.map(player => {
-                const isCurrent = player.playerId === localStorage.getItem(USER_HASH_KEY)
-                return <span className="badge badge-pill badge-secondary"
-                      style={{ 
-                          fontSize: '1.5em', 
-                          marginRight: '0.5em', 
-                          ...(isCurrent ? {
-                            backgroundColor: 'var(--primary)'
-                          }:{
-                              backgroundColor: 'var(--secondary)'
-                          })
-                          }}>
-                      { player.playerId === game.playerId ?   `YOU (${player.name})` : player.name }
-                </span>
-            }) }
+                <p>{ game.players.length } player(s) joined:</p>
+
+                { game.players.map(player => {
+                    const isCurrent = player.playerId === localStorage.getItem(USER_HASH_KEY)
+                    return <span className="badge badge-pill badge-secondary"
+                        style={{ 
+                            fontSize: '1.5em', 
+                            marginRight: '0.5em', 
+                            ...(isCurrent ? {
+                                backgroundColor: 'var(--primary)'
+                            }:{
+                                backgroundColor: 'var(--secondary)'
+                            })
+                        }}>
+                        { player.playerId === game.playerId ?   `YOU (${player.name})` : player.name }
+                    </span>
+                }) }
             </div>
           );
     }
